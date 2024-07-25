c103d8141c88d555ffc7341b8fcfa399c6a28b7a
AI Professionals <aiprofessionals061@gmail.com>
2024-07-26 00:10:20 +0500

    login added

---
+++ resources/views/admin/layouts/header_files.blade.php
@@ -0,0 +1,10 @@
+<link rel="preconnect" href="https://fonts.gstatic.com/">
+<link rel="shortcut icon" href="{{asset('backend/img/icons/icon-48x48.png')}}" />
+
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&amp;display=swap" rel="stylesheet">
+
+<!-- Choose your prefered color scheme -->
+<!-- <link href="css/light.css" rel="stylesheet"> -->
+<!-- <link href="css/dark.css" rel="stylesheet"> -->
+
+<link class="js-stylesheet" href="{{asset('backend/css/light.css')}}" rel="stylesheet">
