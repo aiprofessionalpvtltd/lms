b433a2f290a8fcbe44ad44faa2db0c8b33ed8249
Salman Mehboob <salmanmehboob7@gmail.com>
2024-11-13 13:01:01 +0500

    add is_calculation

--- app/Http/Resources/LoanApplicationTrackingResource.php
+++ app/Http/Resources/LoanApplicationTrackingResource.php
@@ -17,6 +17,7 @@ class LoanApplicationTrackingResource extends JsonResource
         // Check if all conditions are true
         $isApplicationSubmitted = true;
         $hasAttachments = $this->attachments && $this->attachments->isNotEmpty();
+        $hasCalculation = $this->calculatedProduct && $this->calculatedProduct->isNotEmpty();
         $hasGuarantors = $this->guarantors && $this->guarantors->isNotEmpty();
         $isProcessCompleted = ($this->is_submitted == 1)  ? true : false;
 
@@ -32,6 +33,7 @@ class LoanApplicationTrackingResource extends JsonResource
             'is_completed' => ($this->is_completed == 1)  ? true : false,
             'is_application_submitted' => $isApplicationSubmitted,
             'is_documents_uploaded' => $hasAttachments ? true : false,  // Check if there are any attachments
+            'is_calculation' => $hasCalculation ? true : false,  // Check if there are any attachments
             'is_process_completed' => $isProcessCompleted,
             'is_loan_guarantor' => $hasGuarantors,
             'status' => $this->status,
