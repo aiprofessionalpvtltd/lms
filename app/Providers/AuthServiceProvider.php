951789fb677cd23e2313531b88f06c08e6afd7c3
Salman Mehboob <salmanmehboob7@gmail.com>
2024-11-14 10:18:01 +0500

    add auth service provider for token time

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+        Passport::ignoreRoutes();
+
+        // Set token expiration time to 30 minutes
+        Passport::tokensExpireIn(now()->addMinutes(30));
+
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
+
+        Passport::tokensCan([
+            'access-user-admin' => 'Access of User Admin Area',
+        ]);
+    }
+}
+
