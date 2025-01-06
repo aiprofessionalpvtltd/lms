cd01bd17771312d12e9af5e192545eeddfe79398
SALMAN <appflex.developer@gmail.com>
2025-01-06 19:46:13 +0500

    debug

--- resources/views/admin/customer/edit.blade.php
+++ resources/views/admin/customer/edit.blade.php
@@ -839,7 +839,7 @@
                                     <div class="form-group">
                                         <input type="text" name="guarantor_contact_name[]" class="form-control"
                                                placeholder="Guarantor Name 2"
-                                               value="{{ isset($customer->references[1]) ? $customer->references[10]->guarantor_contact_name : '' }}">
+                                               value="{{ isset($customer->references[1]) ? $customer->references[1]->guarantor_contact_name : '' }}">
                                         @if ($errors->has('guarantor_contact_name'))
                                             <span
                                                 class="text-danger">{{ $errors->first('guarantor_contact_name') }}</span>
