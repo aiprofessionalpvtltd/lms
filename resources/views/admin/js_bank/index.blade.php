3f9e5acb0055f00e3fd191582bcb6ff98a946999
AppFlex <>
2025-03-10 10:24:59 +0500

    debug

--- resources/views/admin/js_bank/index.blade.php
+++ resources/views/admin/js_bank/index.blade.php
@@ -21,7 +21,6 @@
     <!-- Content area -->
     <div class="content">
 
-        <!-- Basic datatable -->
         <div class="card">
             <div class="card-header">
                 <div class="col-md-12 mt-5">
@@ -32,22 +31,22 @@
             <div class="card-body">
                 <div class="row">
                     <div class="col-6">
-                        <span><b>Authorization Key : </b> {{env('js_zindagi_client_secret')}}</span>
+                        <span><b>Authorization Key : </b> {{env('JS_ZINDAGI_CLIENT_SECRET')}}</span>
                         <a class="btn btn-info" href="{{route('jszindagi.resetAuth')}}">Click to Reset JS Zindagi
-                            AUthorization</a>
+                            Authorization</a>
                     </div>
 
                     <div class="col-6">
 
-                            @if(env('JS_ZINDAGI_ENCRYPTED_MPIN'))
-                                <span><b> MPIN: </b> {{ env('JS_ZINDAGI_MPIN') }}</span> <br>
-                                <span><b>Encrypted MPIN: </b> {{ env('JS_ZINDAGI_ENCRYPTED_MPIN') }}</span>
-                            @else
-                                <form method="POST" action="{{ route('jszindagi.generate.mpin') }}">
-                                    @csrf
-                                    <button type="submit" class="btn btn-info">Generate MPIN</button>
-                                </form>
-                            @endif
+                        @if(env('JS_ZINDAGI_ENCRYPTED_MPIN'))
+                            <span><b> MPIN: </b> {{ env('JS_ZINDAGI_MPIN') }}</span> <br>
+                            <span><b>Encrypted MPIN: </b> {{ env('JS_ZINDAGI_ENCRYPTED_MPIN') }}</span>
+                        @else
+                            <form method="POST" action="{{ route('jszindagi.generate.mpin') }}">
+                                @csrf
+                                <button type="submit" class="btn btn-info">Generate MPIN</button>
+                            </form>
+                        @endif
 
 
                     </div>
@@ -56,7 +55,6 @@
                 </div>
             </div>
         </div>
-        <!-- /basic datatable -->
 
     </div>
     <!-- /content area -->
